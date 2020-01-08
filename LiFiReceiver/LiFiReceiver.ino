8018476b8029210578f9ecad598958035793794a
Kaloyan Veselinov <kvesselinov@gmail.com>
2020-01-08 17:15:44 +0100

    Sending ACK to bus using I2C

--- LiFiReceiver/LiFiReceiver.ino
+++ LiFiReceiver/LiFiReceiver.ino
@@ -1,28 +1,8 @@
-#ifndef TIMERONE_H
-#define TIMERONE_H
 #include <TimerOne.h>
-#endif
-
-#ifndef EMITTER_H
-#define EMITTER_H
-#include "emitter.h"
-#endif
-
-#ifndef RECEIVER_H
-#define RECEIVER_H
 #include "receiver.h"
-#endif
+#include<Wire.h>
 
-//#ifndef DEBUG
-//#define DEBUG
-//#endif
-
-void isr(){
-  static int i = 0;
-  sample_signal_edge();
-  if(i == 0) emit_half_bit();
-  i = ++i % SAMPLE_PER_SYMBOL;
-}
+#define ACK_CHANNEL 9
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -30,20 +10,9 @@ void setup() {
   Serial.begin(115200);
   Serial.println("Start of receiver program");
   init_receiver();
-  init_emitter();
+  Wire.begin(); 
   Timer1.initialize(SYMBOL_PERIOD/SAMPLE_PER_SYMBOL); //1200 bauds oversampled by factor 4
-  Timer1.attachInterrupt(isr);
-}
-
-char com_buffer [32] ;
-void write_ack_to_buffer(unsigned char crc){
-  static char * msg = "A " ; 
-  static int msg_len = strlen(msg);
-  memcpy(com_buffer, msg, msg_len);
-  com_buffer[msg_len] = crc + '0' ;
-  if(write(com_buffer, strlen(com_buffer)) < 0){
-    delay(10);
-  }
+  Timer1.attachInterrupt(sample_signal_edge);
 }
 
 // the loop routine runs over and over again forever:
@@ -73,10 +42,9 @@ void loop() {
       Serial.print("Acknowledging ");
       Serial.println(crc_to_acknowledge);
       #endif
-      write_ack_to_buffer(crc_to_acknowledge);
-    }
-    else {
-      resend_frame();
+      Wire.beginTransmission(ACK_CHANNEL); // transmit to device ACK_CHANNEL
+      Wire.write(crc_to_acknowledge);
+      Wire.endTransmission();   
     }
   }
 }
